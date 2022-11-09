4065dd1de8e8f49483a5a104a8a9d1c3f176bb46
teja679 <lovlytejaa@gmail.com>
2022-11-09 21:51:25 +0530

    structure building

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import AuthPage from './Components/AuthPage/AuthPage';
+import Applications from './Components/Candidate/Applications';
+import CandidateConversation from './Components/Candidate/CandidateConversation';
+import CandidateJobs from './Components/Candidate/CandidateJobs';
+import CandidateOnboarding from './Components/Candidate/CandidateOnboarding';
+import CandidateProfile from './Components/Candidate/CandidateProfile';
+import LandingPage from './Components/LandingPage/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/auth' element={<AuthPage />} />
+
+        <Route path='/candidateOnboarding' element={<CandidateOnboarding />} />
+        <Route path='candidate/jobs' element={<CandidateJobs />} />
+        <Route path='candidate/conversation' element={<CandidateConversation />} />
+        <Route path='candidate/profile' element={<CandidateProfile />} />
+        <Route path='candidate/applications' element={<Applications />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
